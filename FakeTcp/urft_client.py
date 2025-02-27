d1d2c652a7451b8e43925cf0c1c008a39536f039
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-02-27 11:52:53 +0700

    feat: :sparkles: try exect for exxiting gracefullty

--- FakeTcp/urft_client.py
+++ FakeTcp/urft_client.py
@@ -2,6 +2,7 @@
 Fake TCP Client
 '''
 
+import logging
 from sys import argv
 
 from fake_tcp import Client
@@ -16,8 +17,13 @@ def main():
         print("3 arguments required: filename server_ip server_port")
         return
 
-    client = Client(server_ip=server_ip, server_port=server_port)
-    client.send_file(filename)
+    try:
+        client = Client(server_ip=server_ip, server_port=server_port)
+        client.send_file(filename)
+    except TimeoutError:
+        logging.warning('timeout... bailing')
+    except ConnectionRefusedError:
+        logging.warning('connection closed... bailing')
 
 
 
--- FakeTcp/urft_server.py
+++ FakeTcp/urft_server.py
@@ -9,6 +9,7 @@ checksum: bytes
 data: bytes
 '''
 
+import logging
 from sys import argv
 
 from fake_tcp import Server
@@ -22,8 +23,13 @@ def main():
         print("2 arguments required: server_ip server_port")
         return
 
-    server = Server(server_ip=server_ip, server_port=server_port)
-    server.recieve()
+    try:
+        server = Server(server_ip=server_ip, server_port=server_port)
+        server.recieve()
+    except TimeoutError:
+        logging.warning('timeout... bailing')
+    except ConnectionRefusedError:
+        logging.warning('connection closed... bailing')
 
 
 if __name__ == '__main__':
